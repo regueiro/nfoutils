f0fb6df4683b29af19674bd83379b258bcab7819
Santiago Regueiro <santi@regueiro.es>
2014-01-19 21:21:36 +0100

    Temporarily renamed old classes to get them out of the way

--- src/test/java/es/regueiro/media/FolderScannerTest.java
+++ src/test/java/es/regueiro/media/FolderScannerTest.java
@@ -1,54 +1,54 @@
-package es.regueiro.media;
-
-import static org.junit.Assert.*;
-
-import java.util.Collection;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import es.regueiro.nfoutils.internal.files.FolderScanner;
-import es.regueiro.nfoutils.internal.files.MediaFolderManager;
-import es.regueiro.nfoutils.internal.model.XbmcMovie;
-
-
-public class FolderScannerTest {
-
-	MediaFolderManager manager;
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-		manager = new MediaFolderManager();
-		
-//		manager.addMediaFolder("P:\\Peliculas");
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void test() {
-		//TODO: Create a proper test with temp folders and mocked movie nfos.
-		FolderScanner scanner = new FolderScanner(manager.getMediaFoldersCollection());
-		
-		Collection<XbmcMovie> movies = scanner.scanForMovies();
-
-		for (XbmcMovie movie:movies) {
-			System.out.println(movie);
-		}
-	}
-
-}
+//package es.regueiro.media;
+//
+//import static org.junit.Assert.*;
+//
+//import java.util.Collection;
+//
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//
+//import es.regueiro.nfoutils.internal.files.FolderScanner;
+//import es.regueiro.nfoutils.internal.files.MediaFolderManager;
+//import es.regueiro.nfoutils.internal.model.XbmcMovie;
+//
+//
+//public class FolderScannerTest {
+//
+//	MediaFolderManager manager;
+//
+//	@BeforeClass
+//	public static void setUpBeforeClass() throws Exception {
+//		
+//	}
+//
+//	@AfterClass
+//	public static void tearDownAfterClass() throws Exception {
+//	}
+//
+//	@Before
+//	public void setUp() throws Exception {
+//		manager = new MediaFolderManager();
+//		
+////		manager.addMediaFolder("P:\\Peliculas");
+//	}
+//
+//	@After
+//	public void tearDown() throws Exception {
+//	}
+//
+//	@Test
+//	public void test() {
+//		//TODO: Create a proper test with temp folders and mocked movie nfos.
+//		FolderScanner scanner = new FolderScanner(manager.getMediaFoldersCollection());
+//		
+//		Collection<XbmcMovie> movies = scanner.scanForMovies();
+//
+//		for (XbmcMovie movie:movies) {
+//			System.out.println(movie);
+//		}
+//	}
+//
+//}
